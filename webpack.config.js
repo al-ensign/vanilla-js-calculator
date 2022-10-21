badb3e5341305a86dbbd25ca71a0c07668881e91
alensign <alexandra.wilchitskaya@gmail.com>
2022-10-21 16:17:41 +0300

    update webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,31 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  entry: './index.ts',
+  entry: './src/index.ts',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new ESLintPlugin({
+      emitError: true,
+      emitWarning: true,
+      failOnError: true,
+      extensions: ['.ts', '.tsx', '.js']
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
+        use: 'esbuild-loader',
+        options: {
+            loader: 'ts',
+            target: 'es6',
+            exclude: /node_modules/,
+            include: resolve(__dirname, 'src'),
+        }
       },
     ],
   },
