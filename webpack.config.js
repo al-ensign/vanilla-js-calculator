7eebfc1fea2e7d4b7e8086c4749c71b357cba0ad
alensign <alexandra.wilchitskaya@gmail.com>
2022-10-24 21:46:35 +0300

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: "./src/index.ts",
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: "./src/index.html",
     }),
     new ESLintPlugin({
       emitError: true,
