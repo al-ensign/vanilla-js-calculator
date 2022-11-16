55a6d7d286864ed026ac98c9b896d327e51e9680
alensign <alexandra.wilchitskaya@gmail.com>
2022-11-16 15:25:13 +0100

    add listener to equals

--- src/events/event_listeners.ts
+++ src/events/event_listeners.ts
@@ -1,4 +1,8 @@
 import * as elements from "./dom_elements"
+import { addNum } from "../math_functions/add"
+import { subtractNum } from "../math_functions/subtract"
+import { multiplyNum } from "../math_functions/multiply"
+import { divideByNum } from "../math_functions/divide"
 
 let histNum = ""
 let currNum = ""
@@ -10,12 +14,13 @@ elements.numberElements.forEach((number: HTMLDivElement) => {
   number.addEventListener("click", (e) => {
     e.preventDefault()
 
+    //allows only 1 dot
     if (number.innerText === "." && !haveDot) {
       haveDot = true
     } else if (number.innerText === "." && haveDot) {
       return
     }
-    console.log(e.target)
+    //sets currNum equal to button's value and adds to the current display
     currNum += number.innerText
     elements.currDisplayElement.innerText = currNum
   })
@@ -24,25 +29,73 @@ elements.numberElements.forEach((number: HTMLDivElement) => {
 elements.operationElements.forEach((operation: HTMLDivElement) => {
   operation.addEventListener("click", (e) => {
     e.preventDefault()
-
+    //does nothing if there is no currNum
     if (!currNum) {
       return
     }
+
+    //resets haveDot to false so that we can use float as next operand
     haveDot = false
-    const operationName = operation.innerText
+
+    const operationName = operation.innerHTML
+    //checks if there is data in History/Current displays and lastOperation
     if (histNum && currNum && lastOperation) {
-      //do math
+      computeOperation()
     } else {
       result = parseFloat(currNum)
     }
+    //clear CurrNum and store data in History and Temporary Result displays
     clearCurrentAddHistory(operationName)
+    lastOperation = operationName
+    console.log(lastOperation)
   })
 })
 
+elements.computeElement.addEventListener("click", (e) => {
+  e.preventDefault()
+  if (!currNum || !histNum) {
+    return
+  }
+  haveDot = false
+  computeOperation()
+  clearCurrentAddHistory()
+  elements.currDisplayElement.innerText = result
+  elements.tempResDisplayElement.innerText = ""
+  currNum = result
+  histNum = ""
+})
+
 function clearCurrentAddHistory(name = "") {
+  /* 
+  1) stores currNum and operation in History 
+  2) assigns it to History display
+  3) clears currNum and Current display
+  4) adds value to the Temporary Result display
+  */
   histNum += currNum + " " + name + " "
   elements.histDisplayElement.innerText = histNum
   currNum = ""
   elements.currDisplayElement.innerText = ""
   elements.tempResDisplayElement.innerText = result
+}
+
+function computeOperation() {
+  let prevNum = parseFloat(result)
+  let currentNum = parseFloat(currNum)
+  switch (lastOperation) {
+    case "x":
+      result = multiplyNum(prevNum, currentNum)
+      break
+    case "/":
+      result = divideByNum(prevNum, currentNum)
+      break
+    case "+":
+      result = addNum(prevNum, currentNum)
+      break
+    case "-":
+      result = subtractNum(prevNum, currentNum)
+      break
+    default:
+      return
+  }
 }
